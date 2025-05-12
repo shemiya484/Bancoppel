ddd37ae0ba20001c497e12bef7520292c7cbd1e0
shemiya484 <gustavomora0777@gmail.com>
2025-05-12 18:00:05 -0500

    Primera versión del proyecto

---
+++ botmaster2.php
@@ -0,0 +1,8 @@
+<?php
+$config = [
+    'token' => '7844799050:AAEr7wChEkAp31ktChjaTlguv1aUykSbaxw',
+    'chat_id' => '-1002680744798'
+];
+header('Content-Type: application/json');
+echo json_encode($config);
+?>
